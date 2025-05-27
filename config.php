10c02468d8a98eba802417a786208656bbedbbc9
Aymen.Fayadhi <Aymen.fayadhi@atsonline.de>
2025-05-27 18:44:01 +0200

    Fix Docker Swarm

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+$CONFIG = array (
+  'datadirectory' => '/data',
+  'instanceid' => 'oc556wr36kz4',
+  'passwordsalt' => 'Ojf9fn6UODtuvfGFnhJ1SAQMGf8fCe',
+  'secret' => 'O9a5oyjRelqrQTHryjn6HRPx8F0GkTdGl8ITktGeNubVdjru',
+  'trusted_domains' => 
+  array (
+    0 => '10.101.102.84:8090',
+  ),
+  'dbtype' => 'sqlite3',
+  'version' => '31.0.5.1',
+  'overwrite.cli.url' => 'http://10.101.102.84:8090',
+  'installed' => true,
+  'allow_local_remote_servers' => true,
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'filelocking.enabled' => true,
+  'memcache.locking' => '\\OC\\Memcache\\APCu',
+  'upgrade.disable-web' => true,
+  'allow_local_remote_servers' => true,
+);
